bb47027abf850cfd849ac8dea518fa877376a6e2
jschaefer799 <70353044+jschaefer799@users.noreply.github.com>
2020-11-04 12:38:22 -0600

    Add existing file

--- finalproject/src/main/java/usmanf/models/TotalJobsUtil.java
+++ finalproject/src/main/java/usmanf/models/TotalJobsUtil.java
@@ -10,14 +10,15 @@ public class TotalJobsUtil {
 
     public static Map<TotalJobs, List<CustomDate>> getDateMap(List<CustomDate> dates){
         Map<TotalJobs, List<CustomDate>> grouped = new HashMap<>();
-        for (CustomDate d: dates){
-            for (TotalJobs populationCategory: TotalJobs.values()){
-                if (d.getTotalJobs() >= populationCategory.getMin() && d.getTotalJobs() <= populationCategory.getMax()){
-                    addToMap(populationCategory, d, grouped);
-                }
-            }
-        }
+        dates.forEach(d -> placeValuesInDatesCategories(grouped, d));
         return grouped;
+    }
+
+    private static void placeValuesInDatesCategories (Map<TotalJobs, List<CustomDate>> grouped, CustomDate d){
+        Arrays.asList(TotalJobs.values()).forEach(populationCategory ->{
+            if (d.getTotalJobs() >= populationCategory.getMin() && d.getTotalJobs() <= populationCategory.getMax())
+                addToMap(populationCategory, d, grouped);
+        });
     }
 
     private static void addToMap(TotalJobs key, CustomDate date, Map<TotalJobs, List<CustomDate>> map){
--- finalproject/src/main/java/usmanf/views/ComboBoxDisplays.java
+++ finalproject/src/main/java/usmanf/views/ComboBoxDisplays.java
@@ -1,7 +1,5 @@
 package usmanf.views;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
@@ -15,7 +13,6 @@ import filereader.BufferedReaderOutputFile;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -25,18 +22,16 @@ public class ComboBoxDisplays {
 
     private ComboBox<TotalJobs> categories;
     private ComboBox<CustomDate> dates;
-    private Map<TotalJobs, List<CustomDate>> datesMap;
-    private ObservableList<TotalJobs> totals;
-    private Text textBox;
-    private List<CustomDate> datesList;
-    private Path path;
+    private final Map<TotalJobs, List<CustomDate>> datesMap;
+    private final ObservableList<TotalJobs> totals;
+    private final Text textBox;
     BufferedReaderOutputFile bufferedReaderOutputFile = new BufferedReaderOutputFile();
 
 
     public ComboBoxDisplays() throws URISyntaxException, IOException {
-        path = bufferedReaderOutputFile.getOutputPath();
+        Path path = bufferedReaderOutputFile.getOutputPath();
         bufferedReaderOutputFile.bufferedReaderOutput(path);
-        datesList = bufferedReaderOutputFile.getDates();
+        List<CustomDate> datesList = bufferedReaderOutputFile.getDates();
         datesMap = getDateMap(datesList);
         totals = observableArrayList(datesMap.keySet());
         textBox = new Text();
@@ -44,7 +39,7 @@ public class ComboBoxDisplays {
         setUpDates();
     }
 
-    private class DatesStringConverter extends StringConverter<CustomDate> {
+    private static class DatesStringConverter extends StringConverter<CustomDate> {
 
         private final String SEP = ", Industry NACIS Code: ";
         @Override
@@ -70,16 +65,13 @@ public class ComboBoxDisplays {
     }
 
     private ObservableList<TotalJobs> sortTotals(){
-        return totals.sorted(new Comparator<TotalJobs>(){
-           @Override
-           public int compare(TotalJobs o1, TotalJobs o2){
-               if (o1.getMin() < o2.getMin())
-                   return -1;
-               else if (o1.getMin() > o2.getMin())
-                   return 1;
-               else
-                   return 0;
-           }
+        return totals.sorted((o1, o2) ->{
+            if (o1.getMin() < o2.getMin())
+                return -1;
+            else if (o1.getMin() > o2.getMin())
+                return 1;
+            else
+                return 0;
         });
     }
 
@@ -108,18 +100,15 @@ public class ComboBoxDisplays {
     }
 
     private void createDatesSelectionListener(){
-        dates.valueProperty().addListener(new ChangeListener<CustomDate>(){
-            @Override
-            public void changed (ObservableValue<? extends CustomDate> observable, CustomDate oldValue, CustomDate newValue){
-                if (newValue != null){
-                    String displayText = "Year: " + newValue.getYear() + "\n"
-                            + "NACIS Code: " + newValue.getNacisCode() + "\n"
-                            + "Industry Name: " + newValue.getIndustryName() + "\n"
-                            + "Total Jobs: " + newValue.getTotalJobs();
+        dates.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                String displayText = "Year: " + newValue.getYear() + "\n"
+                        + "NACIS Code: " + newValue.getNacisCode() + "\n"
+                        + "Industry Name: " + newValue.getIndustryName() + "\n"
+                        + "Total Jobs: " + newValue.getTotalJobs();
 
-                    textBox.setText(displayText);
-                    textBox.setVisible(true);
-                }
+                textBox.setText(displayText);
+                textBox.setVisible(true);
             }
         });
     }
@@ -128,14 +117,11 @@ public class ComboBoxDisplays {
         categories = new ComboBox<>();
         categories.getItems().addAll(sortTotals());
         categories.setPromptText("--Select a Value--");
-        categories.valueProperty().addListener(new ChangeListener<TotalJobs>(){
-            @Override
-            public void changed (ObservableValue<? extends TotalJobs> observable, TotalJobs oldValue, TotalJobs newValue){
-                textBox.setVisible(false);
-                dates.getItems().clear();
-                dates.getItems().addAll(datesMap.get(newValue));
-                dates.setVisible(true);
-            }
+        categories.valueProperty().addListener((observable, oldValue, newValue) -> {
+            textBox.setVisible(false);
+            dates.getItems().clear();
+            dates.getItems().addAll(datesMap.get(newValue));
+            dates.setVisible(true);
         });
     }
 
